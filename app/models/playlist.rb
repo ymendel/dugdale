11e1dc477734c26299bb64174c362fcbe8603f36
Yossef Mendelssohn <ymendel@pobox.com>
2008-09-02 17:00:19 -0500

    Playlist loading, writing, and clearing.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -12,4 +12,31 @@ class Playlist
   def full_path
     "#{self.class.root}/#{path}"
   end
+  
+  attr_reader :tracks
+  
+  def initialize
+    load
+  end
+  
+  def load
+    @tracks = []
+    contents = File.read(full_path)
+    contents.split("\n").each do |line|
+      @tracks << Track.new(line.sub(%r%^#{Track.root}/%, ''))
+    end
+  rescue Errno::ENOENT
+  end
+  
+  def write
+    File.open(full_path, 'w') do |f|
+      f.puts tracks.collect(&:full_path).join("\n")
+    end
+  end
+  
+  def clear
+    @tracks = []
+    File.delete(full_path)
+  rescue Errno::ENOENT
+  end
 end
--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -27,32 +27,136 @@ describe Playlist do
     @playlist.full_path.should == "#{Playlist.root}/playlist.m3u"
   end
   
-  it 'should have tracks'
+  it 'should have tracks' do
+    @playlist.should respond_to(:tracks)
+  end
   
-  it 'should load tracks'
+  it 'should load tracks' do
+    @playlist.should respond_to(:load)
+  end
+  
   describe 'loading tracks' do
-    it 'should open the playlist file'
-    describe 'when the playlist file does not exist' do
-      it 'should store an empty track list'
+    before :each do
+      File.stubs(:read).returns('')
     end
+    
+    it 'should open the playlist file' do
+      File.expects(:read).with(@playlist.full_path).returns('')
+      @playlist.load
+    end
+    
+    describe 'when the playlist file does not exist' do
+      before :each do
+        File.stubs(:read).raises(Errno::ENOENT)
+      end
+      
+      it 'should store an empty track list' do
+        @playlist.load
+        @playlist.tracks.should == []
+      end
+    end
+    
     describe 'when the playlist file exists' do
-      it 'should make track objects for the file contents'
-      it 'should store the tracks'
+      before :each do
+        @track_paths = ['track one', 'track two']
+        File.stubs(:read).returns(@track_paths.join("\n"))
+        @tracks = @track_paths.collect { |path|  stub("'#{path}' track") }
+        @track_paths.zip(@tracks).each do |path, track|
+          Track.stubs(:new).with(path).returns(track)
+        end
+      end
+      
+      it 'should make track objects for the file contents' do
+        @track_paths.each do |path|
+          Track.expects(:new).with(path)
+        end
+        @playlist.load
+      end
+      
+      it 'should store the tracks' do
+        @playlist.load
+        @playlist.tracks.should == @tracks
+      end
+      
+      it 'should strip the track root from the beginning of a line' do
+        paths = @track_paths.collect { |path|  "#{Track.root}/#{path}"}
+        File.stubs(:read).returns(paths.join("\n"))
+        @playlist.load
+        @playlist.tracks.should == @tracks
+      end
+      
+      describe 'and is empty' do
+        before :each do
+          File.stubs(:read).returns('')
+        end
+        
+        it 'should store an empty track list' do
+          @playlist.load
+          @playlist.tracks.should == []
+        end
+      end
     end
   end
   
   describe 'when initialized' do
-    it 'should load tracks'
+    it 'should load tracks' do
+      Playlist.any_instance.expects(:load)
+      Playlist.new
+    end
   end
   
-  it 'should clear itself'
+  it 'should clear itself' do
+    @playlist.should respond_to(:clear)
+  end
+  
   describe 'clearing' do
-    it 'should clear its track list'
-    it 'should delete the playlist file'
+    before :each do
+      @playlist.instance_variable_set('@tracks', [1,2,3,4,5])
+      File.stubs(:delete)
+    end
+    
+    it 'should clear its track list' do
+      @playlist.clear
+      @playlist.tracks.should == []
+    end
+    
+    it 'should delete the playlist file' do
+      File.expects(:delete).with(@playlist.full_path)
+      @playlist.clear
+    end
+    
+    describe 'when the file does not exist' do
+      before :each do
+        File.stubs(:delete).raises(Errno::ENOENT)
+      end
+      
+      it 'should not error' do
+        lambda { @playlist.clear }.should_not raise_error
+      end
+    end
   end
   
-  it 'should write'
+  it 'should write' do
+    @playlist.should respond_to(:write)
+  end
+  
   describe 'writing' do
-    it 'should write the track paths to the playlist file'
+    before :each do
+      @track_paths = ['track one', 'track two']
+      @tracks = @track_paths.collect { |path|  stub("'#{path}' track", :full_path => path) }
+      @playlist.instance_variable_set('@tracks', @tracks)
+      @file = stub('file', :puts => nil)
+      File.stubs(:open).yields(@file)
+    end
+    
+    it 'should open the playlist file for writing' do
+      File.expects(:open).with(@playlist.full_path, 'w')
+      @playlist.write
+    end
+    
+    it 'should write the track paths to the playlist file' do
+      @file.expects(:puts).with(@track_paths.join("\n"))
+      @playlist.write
+    end
   end
 end
